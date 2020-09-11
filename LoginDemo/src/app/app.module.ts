6b56f3e29729d69c501d0a73f5f12ac91ffe8347
jomphop <jomphop@perfenterprise.com>
2020-09-11 16:53:47 +0700

    update

--- LoginDemo/src/app/app.module.ts
+++ LoginDemo/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlertComponent } from './_components/alert.component';
+import { AccountComponent } from './_services/account.component';
+import { Account2Component } from './_services/account2.component';
+import { AccountThaiComponent } from './_services/account-thai.component';
+import { Accountthai2Component } from './_services/accountthai2.component';
+import { AccountThai3Component } from './_services/account-thai3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlertComponent,
+    AccountComponent,
+    Account2Component,
+    AccountThaiComponent,
+    Accountthai2Component,
+    AccountThai3Component
   ],
   imports: [
     BrowserModule,
